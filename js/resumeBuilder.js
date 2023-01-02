2d7db021604ea3e7fa1c6bb317c3cb61eddfbd99
duohxx <duohxx@163.com>
2023-01-01 21:02:59 -0330

    my resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,446 @@
+var bio = {
+	"name": "Duo(Stephen) Han",
+	"role": "Software Developer",
+	"contacts": {
+		"email": "duohxx@gmail.com",
+		"github": "StephenTech",
+		"facebook": "@Duo Han",
+		"location": "StJohns, NL, Canada"
+	},
+	"picture": "images/duo.png",
+	"welcomeMessage": "I'm currently a student in Memorial University of Newfoundland. My major is Computer Engineering. This is my " + 
+	"second master degree. I like computer, coding and learning something new. Thanks for stopping by!",
+	"skills": ["Java (Springboot), IDEA/Eclipse", "JavaScript (Node.JS, React, Redux), HTML/CSS (Material UI)", 
+			  "SQL (MySQL), MongoDB", "Python (Tensorflow, PyTorch)", "C/C++ (uC/OS ii, embedded system), Assemble", "Matlab, Git, Linux"]
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "TensorChip Co.,Ltd",
+			"title": "Architecture Design Engineer (Intern)",
+			"location": "Beijing, China",
+			"datesWorked": "08/2020-05/2021",
+			"description": "developed AI acceleration algorithm based on RISC-V architecture under the guidance of experts; \n "+
+				"tested and analyzed algorithm/software; \n"+
+				"compiled builderoot, and upload freeloader and Linux kernel to RISC - V (ux607) development board; \n "+
+				"participated in Wall · E robot project. Wall`E robot project leverages three modules: Raspberry pi, industrial computer and Hikvision camera, to implement the function of tracking people's face. \n",
+			"images": ["images/WallE.jpg"],
+		},
+		{
+			"employer": "Beijing Jingwei Hirain Technologies Co., Inc.",
+			"title": "Embedded Software Engineer (Intern)",
+			"location": "Beijing, China",
+			"datesWorked": "01/2020-05/2020",
+			"description": "assisted in the development of millimeter wave radar, including IWR6843AOP and AWR1642boost Millimeter wave "+
+				"sensors (ARM cortex R4 and DSP C67×dual-core); \n "+
+				"assisted my more experienced colleagues in the scientific research work; ",
+				"images": ["images/hirain.jpg"],
+		},
+		// {
+		// 	"employer": "Enbridge",
+		// 	"title": "Software Developer",
+		// 	"location": "Edmonton, Alberta, Canada",
+		// 	"datesWorked": "September 2008 - March 2015",
+		// 	"description": "My primary responsibilities involve all aspects of developing and supporting critical software " +
+		// 		"applications in a variety of languages including Java, python, C++ and Perl in both greenfield and " +
+		// 		"legacy systems.  This work includes, but is not limited to, database design, software architecture design, high level and " +
+		// 		"detailed estimates as well as peer review of submitted work. I am also very involved in encouraging " +
+		// 	"communication and collaboration within the team and have experience giving technical presentations as well " +
+		// 	"as facilitating project retrospective discussions."
+		// },
+		// {
+		// 	"employer": "Epcor",
+		// 	"title": "Systems Analyst",
+		// 	"location": "Edmonton, Alberta, Canada",
+		// 	"datesWorked": "September 2003 - April 2006",
+		// 	"description": "I worked in a few different capacities here from internal application support of " +
+		// 	"internal users using third party customized applications to working on internal software applications " +
+		// 	"built using Oracle PL/SQL.  I also learned and used PHP to develop an internal web site to facilitate " +
+		// 	"communication between the quality control and development teams by tracking builds, releases and issues."
+		// },
+		// {
+		// 	"employer": "Matrikon",
+		// 	"title": "Programmer",
+		// 	"location": "Edmonton, Alberta, Canada",
+		// 	"datesWorked": "March 2002 - September 2003",
+		// 	"description": "I developed software for external clients in a variety of languages including C#.  This " +
+		// 	"included design, development and deployment of back end systems as well as front end graphical user interfaces."
+		// },
+		// {
+		// 	"employer": "University of Windsor",
+		// 	"title": "Teaching Assisstant / Computer Centre Support / Computer Science Tutor",
+		// 	"location": "Windsor, Ontario, Canada",
+		// 	"datesWorked": "September 1998 - April 2001",
+		// 	"description": "I supervised labs for the second year advanced programming class in C as a teaching " +
+		// 	"assisstant.  At the University Computer Centre I helped students with use of the computing resources " +
+		// 	"available.  As a computer science tutor I aided students with class work and helped them through problems."
+		// },
+		// {
+		// 	"employer": "ABB",
+		// 	"title": "Praktikant",
+		// 	"location": "Baden, Switzerland",
+		// 	"datesWorked": "May 2000 - August 2000",
+		// 	"description": "This was my final four month co-op placement through the University Co-op program.  I " +
+		// 	"designed and implemented a prototype software application using JES (Java Embedded Server) and JMS (Java " +
+		// 	"Messaging System) for communication with a European train system.  I was also involved in a collaborative " +
+		// 	"effort of co-authoring a software patent with my colleagues involving JMS technology."
+		// },
+		// {
+		// 	"employer": "Ontario Power Generation",
+		// 	"title": "Programmer",
+		// 	"location": "Kincardine, Ontario, Canada",
+		// 	"datesWorked": "September 1999 - December 1999",
+		// 	"description": "This was my third four month co-op placement through the University Co-op program. I " +
+		// 	"developed virtual control room displays for operator training at the Kincardine Nuclear Power Plant."
+		// },
+		// {
+		// 	"employer": "Cornerstone Technology",
+		// 	"title": "Software Developer",
+		// 	"location": "Windsor, Ontario, Canada",
+		// 	"datesWorked": "May 1998 - April 1999",
+		// 	"description": "I spent my first and second four month co-op placements here.  During my first placement I " +
+		// 	"completed my given duties in less than half the time expected so I was able to work on developing a " +
+		// 	"prototype of an expert system to design CAD drawings of injection moulds using CLIPS, a rules-based " +
+		// 	"language.  I also had the opportunity to create a database management system using Delphi."
+		// }
+	]
+};
+
+var education = {
+	"schools": [
+		{ "name": "Memorial University of Newfoundland",
+			"datesAttended": "2021 - 2023",
+			"location": "St.John's, NL, Canada",
+			"degree": "Master of Science",
+			"major": "Computer Engineering",
+			"url": "www.mun.ca"
+		},
+		{ "name": "Chinese University of Hong Kong",
+			"datesAttended": "2020 - 2021",
+			"location": "Hong Kong, China",
+			"degree": "Master of Engineering",
+			"major": "Electronic Engineering",
+			"url": "www.cuhk.edu.hk"
+		},
+		{ "name": "Capital Normal University",
+			"datesAttended": "2016 - 2020",
+			"location": "Beijing, China",
+			"degree": "Master of Engineering",
+			"major": "Electronic and Information Engineering",
+			"url": "www.cnu.edu.cn"
+		}
+	],
+	"onlineCourses": []
+};
+
+var projects = {
+	"projects": [
+		{
+			"title": "Online Gomoku",
+			"datesWorked": "2022",
+			"description": "This project is an online Gomoku (five-in-a-row) website. You can play Gomoku with others in this website. The website can "+
+			"evaluate the current situation of Gomoku and calculate the current optimal solution. At the same time, the back-end "+
+			"(Springboot) saves the process of the game, which is convenient for review. You can also chat online. The most significant "+
+			"feature is to achieve the function of man-machine match.",
+			"skills": "skills: Deep Learning, CycleGAN, Pytorch (python), Node.js, HTML/CSS/JavaScript",
+			"images": ["images/Gomoku.png"],
+			"url": "https://github.com/duohxx/OnlineGomoku"
+		},
+		{
+			"title": "Image Painting Style Transfer Website ",
+			"datesWorked": "2021",
+			"description": "This project uses CycleGAN neural network to achieve the style transfer of pictures. You can upload an original photo on the " +
+			"front-end, and then choose the style of painting you want to apply. For example, applying Van Gogh's painting style to a " + 
+			"picture, oil painting style, or kiwifruit painting style (currently only three models are trained). Then click GENERATE button." + 
+			"The front-end will send the picture and corresponding parameters to the back-end, and the back-end will call the AI model to " +
+			"generate a new picture and return it to the front-end for display.",
+			"skills": "skills: Mini-max algorithm, Alpha-beta pruning, Springboot (Java), React, Ajax, SocketIO",
+			"images": ["images/cycleGan.png"],
+			"url": "https://github.com/duohxx/OnlineStyleTransferByCycleGAN"
+		},
+		{
+			"title": "Online Hospital",
+			"datesWorked": "2022",
+			"description": "This is a full stack project with separated front-end and back-end. Users can perform functions such as registration, login, " + 
+			"online medical treatment, real-time chat, and content retrieval through the website.",
+			"skills": "skills: HTML/CSS/JavaScript, React, Node, Ajax, Redux, Material UI, MongoDB, express.JS",
+			"images": ["images/OnlineHospital.png"],
+			"url": "https://github.com/duohxx/OnlineHospital"
+		},
+		{
+			"title": "Research on Simple Circuit Characteristic Analyzer Project",
+			"datesWorked": "2020",
+			"description": "Transplanted ucos ii operating system to the CPU, which is the STM32F1 series microcontroller; \n " + 
+			"Device can measure the input and output resistance and amplifier gain;" + 
+			"Device can calculates and demonstrates the time domain and frequency domain image signal, and can draw the amplitude-frequency characteristic curve in LCD screen;",
+			"skills": "skills: Embedded System, C, UCOSii, Analog Circuit",
+			"images": ["images/ucos.jpg"],
+			"url": "https://github.com/duohxx/A-simple-circuit-characteristic-test-device-based-on-STM32"
+		}
+	]
+};
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+var formattedContactInfo = [];
+formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+formattedContactInfo.push(HTMLfacebook.replace("%data%", bio.contacts.facebook));
+formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+
+if(bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+
+	for(i in bio.skills) {
+		$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+	}
+}
+
+for(i in formattedContactInfo) {
+	$("#topContacts").append(formattedContactInfo[i]);
+	$("#footerContacts").append(formattedContactInfo[i]);
+}
+
+
+function displayWork() {
+
+	if(work.jobs.length > 0) {
+	
+		$("#workExperience").append(HTMLworkStart);
+
+		for(i in work.jobs) {
+			var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+			var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+			var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+			var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[i].datesWorked);
+			var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+
+			var formattedEmployerWorkTitle = formattedEmployer + formattedWorkTitle;
+
+			$(".work-entry:last").append(formattedEmployerWorkTitle);
+			$(".work-entry:last").append(formattedWorkLocation);
+			$(".work-entry:last").append(formattedDatesWorked);
+			$(".work-entry:last").append(formattedWorkDescription);
+
+			for(img in  work.jobs[i].images) {
+				var formattedPWorkImage = HTMLWorkImage.replace("%data%", work.jobs[i].images[img]);
+				$(".work-entry:last").append(formattedPWorkImage);
+			}
+		}
+
+	}
+
+}
+
+displayWork();
+
+
+projects.display = function() {
+	if(projects.projects.length > 0) {
+		for(i in projects.projects) {
+			$("#projects").append(HTMLprojectStart);
+
+			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title).replace("#", projects.projects[i].url);
+			var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].datesWorked);
+			var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+			var formattedProjectSkills = HTMLprojectSkills.replace("%data%", projects.projects[i].skills);
+
+			$(".project-entry:last").append(formattedProjectTitle);
+			$(".project-entry:last").append(formattedProjectDates);
+			$(".project-entry:last").append(formattedProjectDescription);
+			$(".project-entry:last").append(formattedProjectSkills);
+
+			for(img in projects.projects[i].images) {
+				var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[img]);
+				$(".project-entry:last").append(formattedProjectImage);
+			}
+			
+
+		}
+	}
+}
+
+projects.display();
+
+education.display = function() {
+	if(education.schools.length > 0 || education.onlineCourses.length > 0) {
+		for(i in education.schools) {
+			$("#education").append(HTMLschoolStart);
+
+			var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+			var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].datesAttended);
+			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);			
+			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+			// var formattedSchoolMinor = HTMLschoolMinor.replace("%data%", education.schools[i].minor);
+
+			$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+			$(".education-entry:last").append(formattedSchoolDates);
+			$(".education-entry:last").append(formattedSchoolLocation);
+			$(".education-entry:last").append(formattedSchoolMajor);
+			// $(".education-entry:last").append(formattedSchoolMinor);
+		}
+
+		if(education.onlineCourses.length > 0) {
+			$("#education").append(HTMLonlineClasses);
+			for(i in education.onlineCourses) {				
+				$("#education").append(HTMLschoolStart);
+				var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].url);
+				var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+				var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].completed);
+				var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url).replace("#", education.onlineCourses[i].url);
+
+				$(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+				$(".education-entry:last").append(formattedOnlineDates);
+				$(".education-entry:last").append(formattedOnlineURL);
+			}
+		}
+		
+	}
+}
+
+education.display();
+
+// /**
+//  * Skills Chart
+//  */
+// $(function () {
+//     $('#container').highcharts({
+//         chart: {
+//             type: 'scatter',
+//             zoomType: 'xy'
+//         },
+//         title: {
+//             text: 'Skills'
+//         },
+//         xAxis: {
+//             title: {
+//                 enabled: true,
+//                 text: 'Used (year)'
+//             },
+//             startOnTick: true,
+//             endOnTick: true,
+//             showLastLabel: true
+//         },
+//         yAxis: {
+//             title: {
+//                 text: 'Duration (months)'
+//             }
+//         },
+//         legend: {
+//             layout: 'vertical',
+//             align: 'left',
+//             verticalAlign: 'top',
+//             x: 250,
+//             y: 0,
+//             floating: true,
+//             backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+//             borderWidth: 1
+//         },
+//         plotOptions: {
+//             scatter: {
+//                 marker: {
+//                     radius: 5,
+//                     states: {
+//                         hover: {
+//                             enabled: true,
+//                             lineColor: 'rgb(100,100,100)'
+//                         }
+//                     }
+//                 },
+//                 states: {
+//                     hover: {
+//                         marker: {
+//                             enabled: false
+//                         }
+//                     }
+//                 },
+//                 tooltip: {
+//                     headerFormat: '<b>{series.name}</b><br>',
+//                     pointFormat: 'Used in {point.x} for {point.y} month(s)'
+//                 }
+// 			}
+// 		},
+// 		series: [{
+// 				name: 'Ruby',
+// 				color: 'rgba(255, 0, 0, .5)',
+// 				data: [[2017, 4], [2016, 12]]
+// 			},
+// 			{
+// 				name: 'Javascript',
+// 				color: 'rgba(204, 0, 0, .5)',
+// 				data: [[2017, 4], [2016, 12], [2015, 8], [2014, 1]]
+// 			},
+// 			{
+// 				name: 'HTML/CSS',
+// 				color: 'rgba(153, 51, 0, .5)',
+// 				data: [[2017, 4], [2016, 12], [2015, 8], [2014, 5], [2002, 3], [2008, 4], [2005, 2]]
+// 			},
+// 			{
+// 				name: 'Web Development',
+// 				color: 'rgba(0, 153, 102, .5)',
+// 				data: [[2017, 4], [2016, 12], [2015, 8], [2014, 2]]
+// 			},
+// 			{
+// 				name: 'TDD',
+// 				color: 'rgba(0, 102, 51, .5)',
+// 				data: [[2016, 12], [2014, 12], [2013, 12], [2012, 12], [2011, 4]]
+// 			},
+// 			{
+// 				name: 'Java',
+// 				color: 'rgba(223, 83, 83, .5)',
+// 				data: [[2014, 11], [2013, 11], [2012, 11], [2011, 8], [2010, 4], [2009, 12], [1998, 8], [1999, 8], [2000, 4]]
+// 			},
+// 			{
+// 				name: 'python',
+// 				color: 'rgba(119, 152, 191, .5)',
+// 				data: [[2014, 1], [2013, 1], [2012, 1], [2011, 1]]
+// 			},
+// 			{
+// 				name: 'SQL',
+// 				color: 'rgba(119, 152, 80, .5)',
+// 				data: [[2016, 12], [2013, 2]]
+// 			},
+// 			{
+// 				name: 'PL/SQL',
+// 				color: 'rgba(170, 162, 30, .5)',
+// 				data: [[2010, 2], [2006, 4]]
+// 			},
+// 			{
+// 				name: 'C++',
+// 				color: 'rgba(0, 102, 0, .5)',
+// 				data: [[2004, 12], [2003, 6]]
+// 			},
+// 			{
+// 				name: 'C',
+// 				color: 'rgba(0, 102, 0, .5)',
+// 				data: [[1997, 8], [2004, 2], [2008, 4]]
+// 			},
+// 			{
+// 				name: 'Django',
+// 				color: 'rgba(204, 102, 0, .5)',
+// 				data: [[2013, 2.5]]
+// 			},
+// 			{
+// 				name: 'Perl',
+// 				color: 'rgba(210, 200, 0, .5)',
+// 				data: [[2011, 3], [2013, 1], [2012, 1], [2014, 1]]
+// 			}]
+// 	});
+// });
+
+
+// $("#mapDiv").append(googleMap);
+
+
